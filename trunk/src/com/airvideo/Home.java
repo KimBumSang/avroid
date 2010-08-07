58e31100b89aa6b66c58a911d02c2073f6ca30a0
bbosh@alternateinterior.com <bbosh@alternateinterior.com@2c5d04df-1bdd-efaf-010b-db93b9a6c7a1>
2010-08-07 20:13:28 +0000

    Detail screen for videos

--- trunk/src/com/airvideo/Home.java
+++ trunk/src/com/airvideo/Home.java
@@ -3,10 +3,15 @@ package com.airvideo;
 import java.net.URL;
 import java.util.ArrayList;
 
+import com.airvideo.R;
+import com.airvideo.R.id;
+import com.airvideo.R.layout;
+
 import android.app.Activity;
 import android.app.ListActivity;
 import android.app.ProgressDialog;
 import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -26,30 +31,32 @@ public class Home extends ListActivity {
 	private ArrayList <AVResource> items ;
 	private AVFolder pwd;
 	private ProgressDialog _ProgressDialog = null;
-	private Activity activity;
-
+	
 	/** Called when the activity is first created. */
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		this.activity = this;
-		requestWindowFeature(Window.FEATURE_NO_TITLE);
+		requestWindowFeature(Window.FEATURE_NO_TITLE); // turn off tile bar
 		
-		setContentView(R.layout.main);
-		getListView().setOnItemClickListener(listlistener);
+		setContentView(R.layout.main); // specify ui
+		getListView().setOnItemClickListener(listlistener); // click handler
 
-		server = new AVClient("192.168.1.105", 45631, "");
-		pwd = new AVFolder(server, "root", null);
-		items = new ArrayList <AVResource>();
+		server = new AVClient("192.168.1.105", 45631, ""); // server; this should not be hard coded
+		pwd = new AVFolder(server, "root", null); // root folder of server -- this is ok to be hard coded
+		items = new ArrayList <AVResource>(); // perhaps out to be a 'contents' item off the folder
 
-		this._adapter = new AVFolderListAdapter(this, R.layout.row, items);
-		setListAdapter(this._adapter);
+		this._adapter = new AVFolderListAdapter(this, R.layout.row, items); // logic to render items arra
+		setListAdapter(this._adapter); // use that logic
 
+		// get contents of particular folder on seperate thread
+		// this probably ought to be absorbed into AVFolder
 		viewContents = new Runnable() {
 			public void run() {
 				getContents();
 			}
 		};
+		
+		// Get contents of initial folder
 		Thread thread =  new Thread(null, viewContents, "Interrogation");
 		thread.start();
 		_ProgressDialog = ProgressDialog.show(this, "Communication Status", "Interrogating AirVideoServer", true);
@@ -57,17 +64,25 @@ public class Home extends ListActivity {
 
 
 	private void getContents() {
+		// get current directories items
 		items = server.ls(pwd);
+		
+		// Does this run this function (already on its own thread)
+		// in another thread still?
 		runOnUiThread(returnRes);
 	}
 	private Runnable returnRes = new Runnable() {
 		public void run() {
+			// check if resultset is present
 			if(items != null && items.size() > 0){
+				// event callback hook
 				_adapter.notifyDataSetChanged();
+				// rebuild adapter's dataset from supplied dataset
 				_adapter.clear();
 				for(int i=0;i<items.size();i++)
 					_adapter.add(items.get(i));
 			}
+			// done
 			_ProgressDialog.dismiss();
 			_adapter.notifyDataSetChanged();
 		}
@@ -76,19 +91,29 @@ public class Home extends ListActivity {
 		public void onItemClick(AdapterView parent, View arg1, int position, long arg3) {
 			AVResource item = ((AVResource)parent.getItemAtPosition(position));
 			if (item instanceof AVFolder) {
+				// get children
+				_ProgressDialog = ProgressDialog.show(Home.this, "Communication Status", "Getting files in " + item.name, true);
 				pwd = server.cd((AVFolder)item);
 				Thread thread =  new Thread(null, viewContents, "Interrogation");
 				thread.start();
-				_ProgressDialog = ProgressDialog.show(activity, "Communication Status", "Interrogating AirVideoServer", true);
 			} else if (item instanceof AVVideo) {
-				URL url = ((AVVideo)item).live_url();
+				URL url = ((AVVideo)item).url();
 				
-				Context context = getApplicationContext();
-				CharSequence text = url.toString();
-				int duration = Toast.LENGTH_SHORT;
+				Intent i = new Intent(Home.this, DetailVideo.class);
+				try {
+					DetailVideo.video = (AVVideo)item;
+					startActivity(i);
+				} 
+				catch (Exception e) {
+					e.printStackTrace();
+				}
+				
+				//Context context = getApplicationContext();
+				//CharSequence text = url.toString();
+				//int duration = Toast.LENGTH_SHORT;
 
-				Toast toast = Toast.makeText(context, text, duration);
-				toast.show();
+				//Toast toast = Toast.makeText(context, text, duration);
+				//toast.show();
 			}
 		}
 	};
